d8e51822ac15bf0216c998d19df1269a653c76a9
LeoGni <leonardoguadagni2005@gmail.com>
2024-11-17 12:48:50 -0500

    Função cadastrar investidor e excluir investidor

--- painel_admin.c
+++ painel_admin.c
@@ -73,3 +73,78 @@ void menu_administrador() {
     }
   }
 }
+
+// Outras funções
+void cadastrar_investidor() {
+  Investidor investidor;
+  FILE *arquivo = fopen("investidores.txt", "a");
+
+  if (!arquivo) {
+    printf("Erro ao abrir o arquivo de investidores.\n");
+    return;
+  }
+
+  printf("Informe o nome do investidor: ");
+  scanf(" %[^\n]", investidor.nome);
+  printf("Informe o CPF do investidor: ");
+  scanf("%11s", investidor.cpf);
+  printf("Informe a senha de 6 dígitos: ");
+  scanf("%6s", investidor.senha);
+
+  investidor.saldo = 0;
+  investidor.bitcoin = 0;
+  investidor.ethereum = 0;
+  investidor.ripple = 0;
+
+  fprintf(arquivo, "%s,%s,%s,%.2f,%.4f,%.4f,%.4f\n", investidor.nome,
+          investidor.cpf, investidor.senha, investidor.saldo,
+          investidor.bitcoin, investidor.ethereum, investidor.ripple);
+  fclose(arquivo);
+
+  printf("Investidor cadastrado com sucesso.\n");
+}
+void excluir_investidor() {
+  char cpf[12];
+  char confirma;
+  Investidor investidor;
+  FILE *arquivo, *temp;
+
+  printf("Informe o CPF do investidor a ser excluído: ");
+  scanf("%11s", cpf);
+
+  carregar_dados_investidor(&investidor, cpf);
+  if (strlen(investidor.cpf) == 0) {
+    printf("Investidor não encontrado.\n");
+    return;
+  }
+
+  printf("Confirma a exclusão de %s (CPF: %s)? (s/n): ", investidor.nome,
+         investidor.cpf);
+  scanf(" %c", &confirma);
+
+  if (confirma == 's') {
+    arquivo = fopen("investidores.txt", "r");
+    temp = fopen("temp.txt", "w");
+
+    while (fscanf(arquivo, "%[^,],%[^,],%[^,],%lf,%lf,%lf,%lf\n",
+                  investidor.nome, investidor.cpf, investidor.senha,
+                  &investidor.saldo, &investidor.bitcoin, &investidor.ethereum,
+                  &investidor.ripple) != EOF) {
+      if (strcmp(investidor.cpf, cpf) != 0) {
+        fprintf(temp, "%s,%s,%s,%.2f,%.4f,%.4f,%.4f\n", investidor.nome,
+                investidor.cpf, investidor.senha, investidor.saldo,
+                investidor.bitcoin, investidor.ethereum, investidor.ripple);
+      }
+    }
+
+    fclose(arquivo);
+    fclose(temp);
+
+    remove("investidores.txt");
+    rename("temp.txt", "investidores.txt");
+
+    printf("Investidor excluído com sucesso.\n");
+  } else {
+    printf("Operação cancelada.\n");
+  }
+}
