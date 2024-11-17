61f583ed4129c804b00ad432acbdc0e27ea79aea
LeoGni <leonardoguadagni2005@gmail.com>
2024-11-17 12:36:10 -0500

    Declarei as funções relacionadas ao painel admin

--- painel_admin.h
+++ painel_admin.h
@@ -1,1 +1,14 @@
+#ifndef PAINEL_ADMIN_H
+#define PAINEL_ADMIN_H
 
+void login_administrador();
+void menu_administrador();
+void cadastrar_investidor();
+void excluir_investidor();
+void consultar_saldo_investidor();
+void consultar_extrato_admin();
+void cadastrar_criptomoeda();
+void excluir_criptomoeda();
+void atualizar_cotacao();
+
+#endif
